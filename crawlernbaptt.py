26d66f6e5f71dbfb14a9b274791df0acef5e96ce
Kevin <chen880814@gmail.com>
2022-03-06 15:43:36 +0800

    By page

--- crawlernbaptt.py
+++ crawlernbaptt.py
@@ -4,15 +4,21 @@ import time
 
 today = time.strftime('%m/%d').lstrip('0')   # lstrip 移除字串左邊文字
 
-def pttNBA(url):
+def pttNBA(url,num):
 	resp = requests.get(url)
 	if resp.status_code != 200:
 		print('URL Error')
 		return
+	run = 0	
+	page(url, num,run)
 
+def page(url, num, run):
+	if run >= num:
+		return
+		
+	resp = requests.get(url)
 	soup = BeautifulSoup(resp.text, 'html5lib')
 	paging = soup.find('div', 'btn-group btn-group-paging').find_all('a')[1]['href']
-
 	articles = []
 	rents = soup.find_all('div', 'r-ent')
 	for rent in rents:
@@ -20,19 +26,20 @@ def pttNBA(url):
 		count = rent.find('div', 'nrec').text.strip()
 		date = rent.find('div', 'meta').find('div','date').text.strip()
 		article = '%s %s:%s' % (date, count, title)
-
+		  
 		try:
-			if today == date and int(count) > 10:
+			if int(count) > 10:
 				articles.append(article)
 		except:
-			if today == date and count == '爆':
-				articles.append(article)	
+			if count == '爆':
+				articles.append(article)
 
-	if len(articles) != 0:
-		for article in articles:
-			print(article)
-		pttNBA('https://www.ptt.cc' + paging)	
-	else:
-		return
+	for article in articles:
+			print(article)					
+	url = 'https://www.ptt.cc' + paging		
+	page(url, num, run+1)
+	return 
+	
 
-pttNBA('https://www.ptt.cc/bbs/NBA/index.html')
+
+pttNBA('https://www.ptt.cc/bbs/NBA/index.html',5)
